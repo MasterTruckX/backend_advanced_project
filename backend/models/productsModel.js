e12f8b5eb5f1f82f4a4b05407c03cecbd5d2fb28
unknown <jorge.orozco@cetys.edu.mx>
2023-08-24 19:49:55 -0700

    productsModel created

---
+++ backend/models/productsModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please type up the products name']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please type up the products description']
+    },
+    price: {
+        type: Decimal128,
+        required: [true, 'Please type up the products description']
+    },
+    amount: {
+        type: Number,
+        required: [true, 'Please type up the products amount']
+    },
+    available: {
+        type: Boolean,
+        required: true,
+        default: true
+    }
+})
