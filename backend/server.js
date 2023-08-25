8760c264bddd95328a96ac4dc74deb2e4ce7e031
unknown <jorge.orozco@cetys.edu.mx>
2023-08-24 21:32:52 -0700

    errorMiddleware created and ready to use

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
@@ -7,4 +9,10 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use(errorHandler)
+
 app.listen(port, () => console.log(`Server started on port ${port}`))
+
